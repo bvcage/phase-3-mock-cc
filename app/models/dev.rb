8886de81641bb40dec4bcb2ead19daec93380514
Bailey Cage <bailey.cage@gmail.com>
2022-09-28 11:09:32 -0400

    implement dev deliverables

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,15 @@
 class Dev < ActiveRecord::Base
    has_many :freebies
    has_many :companies, through: :freebies
+
+   def received_one?(item_name)
+      self.freebies.find_by(item_name: item_name) ? true : false
+   end
+
+   def give_away(dev, freebie)
+      return if freebie.dev_id != self.id
+      freebie.update(
+         dev_id: dev.id
+      )
+   end
 end
